a2a85c3cba75354530f06c4629e4c27fd2e10fee
vildehys <79218980+vildehys@users.noreply.github.com>
2022-11-17 19:30:18 +0100

    fixed version updates

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -1,7 +1,8 @@
-export const baseURL = "https://nf-api.onrender.com/api/v1";
+export const BASE_URL = "https://nf-api.onrender.com/api/v1";
 
-export const loginURL = baseURL + "/social/auth/login";
-export const registerURL = baseURL + "/social/auth/register";
+export const REGISTER_URL = BASE_URL + "/social/auth/register";
+export const LOGIN_URL = BASE_URL + "/social/auth/login";
 
-export const userURL = baseURL + "/social/profiles";
-export const postsURL = baseURL + "/social/posts";
+export const ALL_POSTS_URL = BASE_URL + "/social/posts";
+
+export const USER_URL = BASE_URL + "/social/profiles";
